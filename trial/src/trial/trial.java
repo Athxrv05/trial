232ee826b614d73d1f047125b2e74b95bf08b98c
Atharv <atharvverlekar80@gmail.com>
2023-04-29 21:10:44 +0530

    done

---
+++ trial/src/trial/trial.java
@@ -0,0 +1,21 @@
+package trial;
+
+public class trial {
+
+	public static void main(String[] args) {
+		{
+			int a,b,sum;
+			
+			a=5;
+			b=3;
+			sum=a+b;
+			//will pull work
+			
+			System.out.println(sum);
+			
+
+		}
+
+	}
+
+}
